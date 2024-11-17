6c03b443f3950f71646fd731d60a6a9319ef8688
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-17 15:01:43 +0100

    Add method to find number

---
+++ HW28/HW28/NumberFinder.cs
@@ -0,0 +1,21 @@
+﻿namespace HW28
+{
+	public class NumberFinder
+	{
+		public static int FindMissingNumber(int[] array)
+		{
+			Array.Sort(array);
+			int iterator = 1;
+
+			foreach (int i in array)
+			{
+				if (i != iterator)
+				{
+					return iterator;
+				}
+				iterator++;
+			}
+			return -1;
+		}
+	}
+}
--- HW28/HW28/Program.cs
+++ HW28/HW28/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HW28;
+
+
+var array = new int[] { 7, 2, 4, 3, 1, 6 };
+Console.WriteLine(NumberFinder.FindMissingNumber(array));
